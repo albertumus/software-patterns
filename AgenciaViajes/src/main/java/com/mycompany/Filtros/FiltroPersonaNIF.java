caad5b26b3ad8fdae3e4df6a058c5c6adc8a90ea
RazvanVC <razvanvc@yahoo.com>
2022-01-30 11:44:28 +0100

    Update FiltroPersonaNIF.java

--- AgenciaViajes/src/main/java/com/mycompany/Filtros/FiltroPersonaNIF.java
+++ AgenciaViajes/src/main/java/com/mycompany/Filtros/FiltroPersonaNIF.java
@@ -1,5 +1,10 @@
 package Filtros;
 
+import com.mycompany.Administracion.Cliente;
+import com.mycompany.Administracion.Empleado;
+import com.mycompany.Administracion.Persona;
+import java.util.ArrayList;
+
 
 /**
  * @author admin
@@ -8,9 +13,19 @@ package Filtros;
  */
 public class FiltroPersonaNIF extends PersonaImp {
 
-	public FiltroPersonaNIF(){
-
+    private ArrayList<Persona> personas;
+	public FiltroPersonaNIF(ArrayList<Persona> personas){
+            this.personas = personas;
 	}
+        
+        public ArrayList<Persona> encontrarPersona(String NIF){
+            ArrayList<Persona> encontrados = new ArrayList ();
+            for (Persona persona : personas){
+                if (persona instanceof Empleado && ((Empleado) persona).getFicha().getDni().equals(NIF)) encontrados.add(persona);
+                else if (persona instanceof Cliente && ((Cliente) persona).getFicha().getDni().endsWith(NIF)) encontrados.add(persona);
+            }
+            return encontrados;
+        }
 
 	public void finalize() throws Throwable {
 		super.finalize();
